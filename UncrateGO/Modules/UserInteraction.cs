fe9d48eefa3b02859fe8eddd93196da83efde76a
Johnny Cai <jaihysc@gmail.com>
2019-04-07 19:23:53 -0400

    Improved initial help menu to search for a channel where it has messaging permissions

--- UncrateGO/Modules/UserInteraction.cs
+++ UncrateGO/Modules/UserInteraction.cs
@@ -1,7 +1,10 @@
-﻿using Discord.Commands;
+﻿using System;
+using Discord.Commands;
 using Discord.WebSocket;
 using System.Linq;
 using System.Threading.Tasks;
+using Discord;
+using UncrateGo.Core;
 
 namespace UncrateGo.Modules
 {
@@ -24,11 +27,34 @@ namespace UncrateGo.Modules
 
         public static async Task SendFirstTimeHelpMenuAsync(SocketGuild socketGuild)
         {
-            //Get first text channel
-            var chnl = socketGuild.TextChannels.FirstOrDefault();
+            try
+            {
+                //Get first text channel
+                var chnlList = socketGuild.TextChannels.ToList();
 
-            //Send initial join message
-            await chnl.SendMessageAsync("**Thank you for adding me! :white_check_mark:**\n`-`My default prefix here is `~`\n`-`View list of commands with `~help`\n`-`You can change my command prefix with `~prefix`\n\n`-`For help with issues or problems: discordapp.com/invite/VNDS9sW");
+                SocketTextChannel messageChannel = null;
+                foreach (var channel in chnlList) //Iterate through the channels to find one where the bot has permissions to message
+                {
+                    foreach (var permissionOverwrite in channel.PermissionOverwrites)
+                    {
+                        if (permissionOverwrite.Permissions.SendMessages == PermValue.Allow)
+                        {
+                            messageChannel = channel;
+                        }
+                    }
+                    //Additionally also if the channel does not have permission overrides try to send in it
+                    if (channel.PermissionOverwrites.Count <= 0) messageChannel = channel;
+                    if (messageChannel != null) break;
+                }
+
+                if (messageChannel != null) await messageChannel.SendMessageAsync("**Thank you for adding me! :white_check_mark:**\n`-`My default prefix here is `~`\n`-`View list of commands with `~help`\n`-`You can change my command prefix with `~prefix`\n\n`-`For help with issues or problems: discordapp.com/invite/VNDS9sW");
+
+            }
+            catch
+            {
+                EventLogger.LogMessage("Unable to send initial help messages", ConsoleColor.Red);
+            }
+
         }
     }
 
